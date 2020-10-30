49ae3fa382acd13b1bec66763e3530b76e230310
NewEXE <v.voloshyn96@gmail.com>
2020-10-30 22:46:44 +0200

    Apply: Hide DevTools and menu bar in production mode, show otherwise

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
-
-
-  
+const Utils = require('./utils');
 
 function createWindow () {
   // Create the browser window.
@@ -18,11 +16,15 @@ function createWindow () {
     }
   })
 
+  // Hide DevTools and menu bar in production mode
+  if (Utils.isPackaged()) {
+    mainWindow.setMenuBarVisibility(false)
+  } else {
+    mainWindow.webContents.openDevTools()
+  }
+
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -38,14 +40,12 @@ app.whenReady().then(() => {
   })
 })
 
-
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
