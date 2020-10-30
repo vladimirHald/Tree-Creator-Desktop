93bb2fe5995b10a6eff3fbe889b0a7c546135e2d
NewEXE <v.voloshyn96@gmail.com>
2020-10-30 22:50:53 +0200

    Add JSDoc

--- utils.js
+++ utils.js
@@ -1,6 +1,11 @@
 const electron = require('electron')
 
 class Utils {
+  /**
+   * Check if Electron is running as compiled (packaged) application.
+   *
+   * @returns {boolean}
+   */
   static isPackaged() {
     if (typeof electron === 'string') {
       throw new TypeError('Not running in an Electron environment!');
