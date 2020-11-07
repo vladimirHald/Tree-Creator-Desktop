a8846c0fe2bea3c71c9fea03fc19fe2223b11d00
NewEXE <v.voloshyn96@gmail.com>
2020-11-08 00:53:19 +0200

    Remove old code

--- utils.js
+++ utils.js
@@ -27,55 +27,10 @@ class Utils {
    */
   static getDirContentAsString(dirPath, options = {}, _privateOptions = { level: 0, dirTree: '', isSep: true }) {
     let dir = new Directory(dirPath);
+
     console.log(dir);
 
-    return 'Temporary disabled.';
-
-    if (_privateOptions.level === 0) {
-      _privateOptions.dirTree += '.'
-    }
-
-    fs.readdirSync(dirPath)
-      .map(dirItemName => {
-        let dirItemPath = dirPath + path.sep + dirItemName;
-        return {
-          name: dirItemName,
-          path: dirItemPath,
-          isDir: fs.lstatSync(dirItemPath).isDirectory()
-        };
-      })
-      .sort((a, b) => {
-        if (a.isDir && !b.isDir) return -1;
-        if (!a.isDir && b.isDir) return 1;
-        return a.name.localeCompare(b.name);
-      })
-      .forEach((dirItem, index, dirItems) => {
-        let dirItemPath = dirItem.path;
-        //let spaces = ' '.repeat(_privateOptions.level * 4);
-        let spaces
-        if(_privateOptions.isSep)
-         spaces = '|  '.repeat(_privateOptions.level);
-        else 
-         spaces = '   '.repeat(_privateOptions.level);
-
-        let isLastItem = index === dirItems.length - 1;
-
-        let startSymbol = isLastItem ? `└` : `├`;
-        _privateOptions.dirTree += `\n${spaces}${startSymbol}── ${dirItem.name}`
-
-        if (dirItem.isDir) {
-          ++_privateOptions.level;
-          if(isLastItem) {
-            _privateOptions.isSep = false
-          } else {
-            _privateOptions.isSep = true
-          }
-          Utils.getDirContentAsString(dirItemPath, options, _privateOptions);
-          --_privateOptions.level;
-        }
-    });
-
-    return _privateOptions.dirTree;
+    return dir.getDirContentAsString();
   }
 
   static getLastIndex(paths) {
